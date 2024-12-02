3ee7996898f43fc57df3fcb563e83a63dc1131bb
ulyssess2140 <91745039+ulyssess2140@users.noreply.github.com>
2024-12-02 04:10:19 +0000

    Fix bug 1

--- Java/Deck.java
+++ Java/Deck.java
@@ -7,7 +7,7 @@ public class Deck {
     public Deck() {
         cards = new ArrayList<>();
         String[] suits = {"Cookies", "Candy Canes", "Presents", "Snowflakes"};
-        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack Frost", "Mrs. Claus", "Santa Claus", "Christmas Miracle"};
+        String[] ranks = {"Elf", "Snowman", "Reindeer", "Angel", "Ornament", "Bell", "Star", "Wish", "List", "Jack Frost", "Mrs. Claus", "Santa Claus", "Christmas Miracle"};
         int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
 
         for(String suit: suits) {
